17bff06ef747a7a7a5dc34084a790909ea74a257
Nikhil <nikhilthakur1509@gmail.com>
2023-11-02 23:35:36 +0530

    first commit

--- first.java
+++ first.java
@@ -1,12 +1,14 @@
-class Main {
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    
-    int first = 10;
-    int second = 30;
+public class first {
+    public static void main(String[] args) {
+        int n;
+        char s1, s2, s3;
+        Scanner in = new Scanner(System.in);
 
-    // add two numbers
-    int sum = first + second;
-    System.out.println(first + " + " + second + " = "  + sum);
-  }
+        System.out.print("Input number: ");
+        n = in.nextInt();
+        
+        System.out.printf("%d\n", n+n*n+n*n*n);
+    }
 }
