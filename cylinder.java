bc8c0cbed720c2c645d3c83dfa4e7aa1314dc119
Nikhil <nikhilthakur1509@gmail.com>
2023-11-02 23:57:23 +0530

    second commit

---
+++ cylinder.java
@@ -0,0 +1,10 @@
+public class cylinder{  
+    public static void main(String args[])  
+    {  
+    int height=1;  
+    int radius=5;  
+    double pie=3.14285714286;  
+    double volume=pie*(radius*radius)*height;  
+        System.out.println("Volume of the cylinder="+volume);  
+     }  
+}  
